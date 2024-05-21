53a61a1390c5ceaf2fc97cf1bc005f5982fe58d4
Sabin Pandey <sabeenpandey2@gmail.com>
2024-05-21 18:49:35 +0545

    resume update

--- src/pages/portfolio/Hero.tsx
+++ src/pages/portfolio/Hero.tsx
@@ -20,7 +20,15 @@ const Hero = () => {
                 </p>
               </div>
               <div className="flex flex-col gap-2 min-[400px]:flex-row items-center">
-                <button className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
+                <button
+                  onClick={() =>
+                    window.open(
+                      "https://drive.google.com/file/d/1TnWOxa51LsapQS9mZ1zyio-hh-UyxQyZ/view",
+                      "_blank"
+                    )
+                  }
+                  className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                >
                   View Resume
                 </button>
                 <button
